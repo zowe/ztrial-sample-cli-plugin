186a18d3962347d51a98e6886c5aebae5e65b838
Nakul Manchanda <nakul.manchanda@ibm.com>
2019-05-08 18:18:22 -0500

    updated hostname
    Signed-off-by: Nakul Manchanda <nakul.manchanda@ibm.com>

--- src/api/Accounts.ts
+++ src/api/Accounts.ts
@@ -19,7 +19,7 @@ const rp = axios.create({
     responseType: "json"
   });
 
-const baseURI: string = "https://10.149.60.171:7554/api/v1/sample-node-api/cli";
+const baseURI: string = "https://s0w1:7554/api/v1/sample-node-api/cli";
 
 export class Accounts {
 
